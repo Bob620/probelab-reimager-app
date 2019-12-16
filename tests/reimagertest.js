a0234b407fcec0ea96355511ed8f32d0a5637061
EPMA_Castaing <bruder.kraft225@gmail.com>
2019-12-16 14:58:26 -0600

    feat(reimagertest) basic reimager backend test

---
+++ tests/reimagertest.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+
+const generateUuid = require('../src/backend/generateuuid');
+const Reimager = require('../src/backend/children/reimager/child.js');
+
+class FakeComm {
+	constructor() {
+		this.data = {
+			one: {
+				listeners: new Map()
+			},
+			two: {
+				listeners: new Map()
+			}
+		}
+	}
+
+	get endOne() {
+		return {
+			send: this.send.bind(this, 'two'),
+			on: this.on.bind(this, 'one')
+		}
+	}
+
+	get endTwo() {
+		return {
+			send: this.send.bind(this, 'one'),
+			on: this.on.bind(this, 'two')
+		}
+	}
+
+	send(end, message) {
+		const listeners = this.data[end].listeners.get('message');
+		if (listeners)
+			for (const listener of listeners)
+				listener(message);
+	}
+
+	on(end, event, listener) {
+		if (typeof listener === 'function') {
+			const listeners = this.data[end].listeners.get(event);
+			if (listeners)
+				listeners.push(listener);
+			else
+				this.data[end].listeners.set(event, [listener]);
+		}
+	}
+}
+
+const fakeComm = new FakeComm();
+
+const outsideComm = fakeComm.endTwo;
+
+outsideComm.on('message', ({type, data, uuid}) => {
+	console.log(type + '    ' + uuid);
+	console.log(data);
+	console.log();
+
+	if (type === 'resolve')
+		process.exit();
+});
+
+const reimager = new Reimager(fakeComm.endOne);
+
+outsideComm.send({
+	type: 'getDir',
+	uuid: generateUuid.v4(),
+	data: {
+		uri: 'C:\\Users\\EPMA_Castaing\\work\\thermo imaging\\2019-08-12_Nolen - Copy'
+	}
+});
