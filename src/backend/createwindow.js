14537c1c94e08495676aac20cd4140d8122c4361
bob620 <bruder.kraft225@gmail.com>
2020-10-11 18:29:06 -0500

    feat(devmode) added basic devmode

--- src/backend/createwindow.js
+++ src/backend/createwindow.js
@@ -1,89 +1,54 @@
-const { BrowserWindow, app, Menu, shell } = require('electron');
+const {BrowserWindow, app, Menu, shell} = require('electron');
 
 const isMac = process.platform === 'darwin';
+const isDev = require('../../package.json').version.includes('dev');
 
 const template = [
-	// { role: 'appMenu' }
-	...(isMac ? [{
+	(isMac ? {
 		label: app.getName(),
 		submenu: [
-			{ role: 'about' },
-			{ type: 'separator' },
-			{ role: 'services' },
-			{ type: 'separator' },
-			{ role: 'hide' },
-			{ role: 'hideothers' },
-			{ role: 'unhide' },
-			{ type: 'separator' },
-			{ role: 'quit' }
+			{role: 'about'},
+			{type: 'separator'},
+			{role: 'services'},
+			{type: 'separator'},
+			{role: 'hide'},
+			{role: 'hideothers'},
+			{role: 'unhide'},
+			{type: 'separator'},
+			{role: 'quit'}
 		]
-	}] : []),
-	// { role: 'fileMenu' }
+	} : {}),
 	{
 		label: 'File',
 		submenu: [
-			isMac ? { role: 'close' } : { role: 'quit' }
+			isMac ? {role: 'close'} : {role: 'quit'}
 		]
 	},
-	// { role: 'editMenu' }
-/*	{
-		label: 'Edit',
-		submenu: [
-			{ role: 'undo' },
-			{ role: 'redo' },
-			{ type: 'separator' },
-			{ role: 'cut' },
-			{ role: 'copy' },
-			{ role: 'paste' },
-			...(isMac ? [
-				{ role: 'pasteAndMatchStyle' },
-				{ role: 'delete' },
-				{ role: 'selectAll' },
-				{ type: 'separator' },
-				{
-					label: 'Speech',
-					submenu: [
-						{ role: 'startspeaking' },
-						{ role: 'stopspeaking' }
-					]
-				}
-			] : [
-				{ role: 'delete' },
-				{ type: 'separator' },
-				{ role: 'selectAll' }
-			])
-		]
-	},
-*/
-/*
-	// { role: 'viewMenu' }
-	{
+	(isDev ? {
 		label: 'View',
 		submenu: [
-			{ role: 'reload' },
-			{ role: 'forcereload' },
-			{ role: 'toggledevtools' },
-			{ type: 'separator' },
-			{ role: 'resetzoom' },
-			{ role: 'zoomin' },
-			{ role: 'zoomout' },
-			{ type: 'separator' },
-			{ role: 'togglefullscreen' }
+			{role: 'reload'},
+			{role: 'forcereload'},
+			{role: 'toggledevtools'},
+			{type: 'separator'},
+			{role: 'resetzoom'},
+			{role: 'zoomin'},
+			{role: 'zoomout'},
+			{type: 'separator'},
+			{role: 'togglefullscreen'}
 		]
-	},
-*/
-	// { role: 'windowMenu' }
+	} : {}),
 	{
 		label: 'Window',
 		submenu: [
-			{ role: 'minimize' },
+			{role: 'minimize'},
 			...(isMac ? [
-				{ type: 'separator' },
-				{ role: 'front' },
-				{ type: 'separator' },
-				{ role: 'window' }
+				{type: 'separator'},
+				{role: 'front'},
+				{type: 'separator'},
+				{role: 'window'}
 			] : [
-				{ role: 'close' }
+				{role: 'close'}
 			])
 		]
 	},
@@ -118,24 +83,25 @@ const template = [
 	}
 ];
 
-const menu = Menu.buildFromTemplate(template);
-Menu.setApplicationMenu(menu);
+Menu.setApplicationMenu(Menu.buildFromTemplate(template.filter(e => e.label || e.role)));
 
 module.exports = log => {
 	log.info('Creating window...');
+	if (isDev)
+		log.info('Window running in Dev mode');
 
 	let window = new BrowserWindow({
-		minWidth: 1100,
-		minHeight: 800,
+		minWidth: 0,  // 1060, down from 1100
+		minHeight: 0, // 680, down from 800
 		backgroundColor: '#4b6584',
 		width: 1100,
 		height: 800,
-		title: 'Probelab ReImager',
+		title: `Probelab ReImager${isDev ? ' Dev' : ''}`,
 		webPreferences: {
 			nodeIntegration: true,
 			enableRemoteModule: false
 		},
-		show: false,
+		show: false
 	});
 
 	window.loadFile('./assets/index.html');
